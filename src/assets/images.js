a15958b05047442ff118a64ba2c080fb9f99707a
Doston21-wq <rajabovdoston500@gmail.com>
2025-07-21 18:44:05 +0500

    add

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,7 +1,8 @@
 import Logo from "./Logo.svg";
-
+import ErrorImage from "./ErrorImage.svg";
 const images = {
   Logo,
+  ErrorImage,
 };
 
 export default images;
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,34 +1,42 @@
-import { useRouteError, isRouteErrorResponse, Link } from 'react-router-dom'
-
-const Error = () => {
-  const error = useRouteError()
-
-  if (isRouteErrorResponse(error) && error.status === 404) {
-    return (
-      <main className='grid min-h-screen place-items-center px-8'>
-        <div className="text-center">
-          <p className='text-9xl font-semibold text-primary'>404</p>
-          <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>
-            Page not found
-          </h1>
-          <p className='mt-6 text-lg leading-7'>
-            Sorry, we couldn't find the page you're looking for.
-          </p>
-          <div className="mt-10">
-            <Link to='/' className='btn btn-secondary uppercase'>
-              Go back home
-            </Link>
-          </div>
-        </div>
-      </main>
-    )
-  }
+import { useNavigate } from "react-router-dom";
+import images from "../assets/images";
+const NotFoundPage = () => {
+  const navigate = useNavigate();
 
   return (
-    <main className='grid min-h-screen place-items-center px-8'>
-      <h4 className='text-center font-bold text-4xl'>There was an error...</h4>
-    </main>
-  )
-}
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-4 text-center">
+      <div className="max-w-md w-full space-y-6">
+       
+        
+        <h2 className="text-2xl font-semibold text-gray-700">
+          Страница не найдена
+        </h2>
+        
+<img src={images.ErrorImage} alt="" className="w-100 mx-auto"/>
 
-export default Error
+        <p className="text-gray-600">
+          Запрашиваемая страница не найдена. Возможно она была удалена, либо её адрес был изменен. 
+          Попробуйте воспользоваться поиском.
+        </p>
+        
+        <div className="flex flex-col sm:flex-row gap-4 justify-center">
+          <button
+            onClick={() => navigate(-1)}
+            className="px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded-md text-gray-700 transition-colors"
+          >
+            Вернуться назад
+          </button>
+          
+          <button
+            onClick={() => navigate("/")}
+            className="px-4 py-2 bg-blue-600 hover:bg-blue-700 rounded-md text-white transition-colors"
+          >
+            На главную
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
