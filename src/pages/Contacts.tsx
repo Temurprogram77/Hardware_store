735c7143bcf2c4a7013e09ddf3141ebdce037072
Бобур <Bosbacl-Bobur@gmail.com>
2025-07-22 14:57:02 +0500

    add

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,7 +1,7 @@
 const Contacts = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Контакты</h1>
+    <div className="p-4 sm:p-6 md:p-10 lg:p-10 mt-4">
+      <h1 className="text-[40px] font-semibold">Контакты</h1>
       <div>
         <div>
          <iframe className="border-0 rounded-[10px]" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1574993.114298276!2d66.8964759!3d39.5560352!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1753176936343!5m2!1sen!2s" width="1300" height="550" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
