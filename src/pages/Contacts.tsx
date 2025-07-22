eb0046e1e05d156dae7fa5035cd0f2136aa052bd
Бобур <Bosbacl-Bobur@gmail.com>
2025-07-22 14:12:15 +0500

    add

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -4,8 +4,7 @@ const ts = () => {
       <h1>Контакты</h1>
       <div>
         <div>
-            <canvas width="1117" height="489"></canvas>
-
+            <canvas width="1366" height="381" id="" className="width=[1366px] height=[381px]" /*tabindex="-1"*/></canvas>
         </div>
         <div></div>
       </div>
