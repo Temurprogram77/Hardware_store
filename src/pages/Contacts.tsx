2c7bcde2e5971ae072c2b801fcf63ecf57d2cc5d
Бобур <Bosbacl-Bobur@gmail.com>
2025-07-22 14:45:12 +0500

    iframe qushdim

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -4,8 +4,8 @@ const ts = () => {
       <h1 className="h1">Контакты</h1>
       <div>
         <div>
-          
-            <canvas width="1366" height="381" id="" className="width=[1366px] height=[381px]" /*tabindex="-1"*/></canvas>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1574993.114298276!2d66.8964759!3d39.5560352!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1753176936343!5m2!1sen!2s" width="600" height="450" className="border-0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
         </div>
         <div></div>
       </div>
