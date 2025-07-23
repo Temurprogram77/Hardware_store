367f9b5ef7894931fa2fae7c774875bea68a1833
Temur Programmer <temurnarzullayev810@gmail.com>
2025-07-23 08:13:45 +0300

    advantage part finished

--- src/components/Advantages.tsx
+++ src/components/Advantages.tsx
@@ -1,22 +1,27 @@
-import {} from "../assets/images"
+import { images } from "../assets/images";
 
+const { advantage1, advantage2, advantage3, advantage4 } = images;
 const Advantages = () => {
   return (
-    <div className="flex items-center">
-        <div className="flex">
-            <img src="" alt="" />
-        </div>
-        <div className="flex">
-            <img src="" alt="" />
-        </div>
-        <div className="flex">
-            <img src="" alt="" />
-        </div>
-        <div className="flex">
-            <img src="" alt="" />
-        </div>
+    <div className="my-10 flex items-center justify-between max-w-[1460px] mx-auto">
+      <div className="flex items-center gap-4">
+        <img src={advantage1} alt="advantage img" />
+        <p className="!m-0">Оплата любым удобным способом</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <img src={advantage2} alt="advantage img" />
+        <p className="!m-0">Большой выбор товаров в каталоге</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <img src={advantage3} alt="advantage img" />
+        <p className="!m-0">Осуществляем быструю доставку</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <img src={advantage4} alt="advantage img" />
+        <p className="!m-0">Делаем скидки на крупные покупки</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Advantages
+export default Advantages;
