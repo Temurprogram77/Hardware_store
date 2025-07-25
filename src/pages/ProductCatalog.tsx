4292b869caf32602bb6e3f1e18a3b0db9c3e237e
Temur Programmer <temurnarzullayev810@gmail.com>
2025-07-25 17:10:00 +0300

    added

--- src/pages/ProductCatalog.tsx
+++ src/pages/ProductCatalog.tsx
@@ -1,21 +1,22 @@
 import data from "../data/data";
 import { images } from "../assets/images";
+import { Link } from "react-router-dom";
 
 const { cartt, heart, compare } = images;
 
 const ProductCatalog = () => {
   return (
-    <div className="max-w-[1460px] mx-auto">
-      <h2 className="text-2xl font-bold mb-6">Стройоптторг</h2>
+    <div className="max-w-[1460px] mx-auto my-5">
+      <div className="text-[13px] !font-medium mb-6 flex items-center gap-3"><Link to={"/"}>Стройоптторг</Link> / <Link to={""}>Электроинструмент</Link></div>
       <div className="flex flex-col lg:flex-row gap-6">
         <div className="lg:w-1/4 w-full bg-gray-100 p-4 rounded">
           <p className="font-semibold">Bu alohida qism</p>
         </div>
         <div className="flex flex-col">
           <div className="flex justify-between items-center my-3">
-            <div className="flex items-center">
+            <div className="flex items-center gap-3">
               <p className="!m-0">Сортировка:</p>
-              <select id="">
+              <select className="border-[#DFE0E2] border outline-none px-4 py-2.5 rounded-md" id="">
                 <option value="">Цена по убыванию</option>
               </select>
             </div>
@@ -57,7 +58,7 @@ const ProductCatalog = () => {
                   </p>
                 </div>
                 <div className="mt-3 flex items-center justify-between">
-                  <div className="flex gap-3 bg-[#186FD4] text-white w-fit px-5 py-2.5 rounded-md">
+                  <div className="flex gap-3 hover:bg-[#000] duration-200 bg-[#186FD4] text-white w-fit px-5 py-2.5 rounded-md">
                     <img src={cartt} alt="cart" />
                     <span>Купить</span>
                   </div>
