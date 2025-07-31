664bca7672e223dd2ea596f5f3b8f1e4a4aa8a41
BekjonCoder <k75154256@gmail.com>
2025-07-31 21:02:13 +0500

    add

--- src/components/CardBgImages.tsx
+++ src/components/CardBgImages.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { images } from "../assets/images";
-import CustomButton from "./ui/CustomButton";
 import { useNavigate } from "react-router-dom";
 const { cardBgImage1, cardBgImage2, cardBgImage3, cardBgImage4 } = images;
 interface stock{
