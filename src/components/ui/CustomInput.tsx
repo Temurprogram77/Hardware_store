f71a61bfbe19516de0c2c9f88d29a258b1b10dc5
asilbekCoder <asilbekegamnazarov999@gmail.com>
2025-08-01 10:59:48 +0500

    add

--- src/components/ui/CustomInput.tsx
+++ src/components/ui/CustomInput.tsx
@@ -4,12 +4,14 @@ import { Input } from 'antd'
 interface Props {
   type: string,
   className?: string,
-  placeholder?: string
+  placeholder?: string,
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  value?: string | number
 }
 
-const CustomInput: React.FC<Props> = ({ type, className, placeholder }) => {
+const CustomInput: React.FC<Props> = ({ type, className, placeholder, onChange, value }) => {
   return (
-    <Input type={type} className={className} placeholder={placeholder} />
+    <Input type={type} className={className} placeholder={placeholder} onChange={onChange} value={value} />
   );
 };
 
