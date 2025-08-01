35310be0138acc8a1519673f2f240daabcb33aed
Doston21-wq <rajabovdoston500@gmail.com>
2025-08-01 15:52:59 +0500

    ADD

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -96,12 +96,9 @@ import Vector10 from "./Vector (10).svg"
 import heart2 from "./heart2.svg"
 import ciziq from "./ciziq.svg"
 import mapCard1 from "./mapCard1.jpg"
-<<<<<<< HEAD
 import buy from './buy.svg'
-=======
 import mapCard2 from "./mapCard2.jpg"
 
->>>>>>> 517eda62ef6fa05bb5d622927a2f2386e481f06c
 export const images = {
   heart2,
   Vector10,
