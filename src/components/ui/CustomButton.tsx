02b8e87bbe59c1c6be1fde13682cae6965bc6213
asilbekCoder <asilbekegamnazarov999@gmail.com>
2025-08-01 10:49:15 +0500

    add

--- src/components/ui/CustomButton.tsx
+++ src/components/ui/CustomButton.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { type ReactNode } from 'react'
 import { Button } from 'antd'
 
 interface Props {
-  type:'primary' | 'default' | 'dashed' | 'link' | 'text',
+  type: 'primary' | 'default' | 'dashed' | 'link' | 'text',
   className?: string,
-  text:string
+  text: string,
+  children?: ReactNode,
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-const CustomButton: React.FC<Props> = ({ type, className,text }) => {
+const CustomButton: React.FC<Props> = ({ type, className, text, children, onClick }) => {
   return (
-    <Button type={type} className={className}>{text}</Button>
+    <Button type={type} onClick={onClick} className={className}>{text}{children}</Button>
   );
 };
 
--- src/components/ui/CustomInput.tsx
+++ src/components/ui/CustomInput.tsx
@@ -4,10 +4,10 @@ import { Input } from 'antd'
 interface Props {
   type: string,
   className?: string,
-  placeholder?:string
+  placeholder?: string
 }
 
-const CustomInput: React.FC<Props> = ({ type, className,placeholder }) => {
+const CustomInput: React.FC<Props> = ({ type, className, placeholder }) => {
   return (
     <Input type={type} className={className} placeholder={placeholder} />
   );
