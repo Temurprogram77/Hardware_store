9014e8b3876ac62e3b1fba1779e5b2afb8f61ebe
BekjonCoder <k75154256@gmail.com>
2025-08-04 15:55:43 +0500

    add

---
+++ src/types/Review.ts
@@ -0,0 +1,12 @@
+export  interface Review {
+  key: number,
+  quiz: string,
+  answer: string,
+}
+
+export interface Review2 {
+  name: string,
+  date: string,
+  text: string,
+  images: string[],
+}
