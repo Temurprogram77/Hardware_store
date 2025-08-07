b4ddddce03f67ef0355fe1b4a59bfd8944498341
Temur Programmer <temurnarzullayev810@gmail.com>
2025-08-07 13:14:38 +0300

    added

---
+++ src/components/ui/CustomSwiper.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+type CustomSwiperProps = {
+  slides: React.ReactNode[];
+  loop?: boolean;
+  autoplay?: boolean;
+  pagination?: boolean;
+  navigation?: boolean;
+  spaceBetween?: number;
+  slidesPerView?: number;
+  breakpoints?: any;
+};
+
+const CustomSwiper: React.FC<CustomSwiperProps> = ({
+  slides,
+  loop = true,
+  autoplay = true,
+  pagination = true,
+  navigation = true,
+  spaceBetween = 20,
+  slidesPerView = 1,
+  breakpoints,
+}) => {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      loop={loop}
+      autoplay={autoplay ? { delay: 3000, disableOnInteraction: false } : false}
+      pagination={pagination ? { clickable: true } : false}
+      navigation={navigation}
+      spaceBetween={spaceBetween}
+      slidesPerView={slidesPerView}
+      breakpoints={breakpoints}
+    >
+      {slides.map((slide, index) => (
+        <SwiperSlide key={index}>{slide}</SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default CustomSwiper;
