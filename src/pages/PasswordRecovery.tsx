27cc37a2deabefa09ddcedb58c9b5adc9566b86b
Asilbek <asilbekegamnazarov999@gmail.com>
2025-08-18 15:47:51 +0500

    add

--- src/pages/PasswordRecovery.tsx
+++ src/pages/PasswordRecovery.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { Breadcrumb, Button, Input, Space, Typography } from "../link/antLink";
-import { Link } from "../link/links";
+import { Button, Space, Typography } from "../link/antLink";
 import type { FC } from 'react';
+import Names from "../components/ui/Names";
+import CustomInput from "../components/ui/CustomInput";
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
@@ -26,30 +27,11 @@ const PasswordRecovery: FC = () => {
     <main className="mb-10 mt-5 px-3">
       <hr className='text-gray-200' />
       <div className="max-w-[1460px] mx-auto mt-2.5">
-        <Breadcrumb
-          items={[
-            {
-              title: (
-                <Link to="/">
-                  <span className='text-black text-sm'>Стройоптторг</span>
-                </Link>
-              ),
-            },
-            {
-              title: <span className='text-[#959597] text-sm'>Восстановление пароля</span>,
-            },
-          ]}
-        />
+        <Names link="" name="Восстановление пароля" />
         <div className="mt-4">
           <h2 className="!font-bold text-4xl sm:text-5xl">Восстановление пароля</h2>
           <div className="!min-h-[400px] flex items-center justify-center">
             <div className="border border-gray-200 flex items-center justify-center flex-col p-10 w-full max-w-lg rounded-lg">
-
-              {error && (
-                <div className='border border-[#E52B0E] p-3 rounded border-dashed bg-[#FFF9F9] mb-5 w-full h-15 flex flex-col items-center justify-center'>
-                  <p className="text-[#E52B0E]">{error}</p>
-                </div>
-              )}
 
               <p className="w-[90%] text-gray-600 leading-7 text-md text-center">
                 <span className="!font-bold">Забыли свой пароль?</span> Укажите свой Email или имя пользователя. Ссылку на создание нового пароля вы получите по электронной почте.
@@ -58,14 +40,15 @@ const PasswordRecovery: FC = () => {
                 <Typography.Title className='!text-lg'>
                   Email <span className='text-red-700'>*</span>:
                 </Typography.Title>
-                <Input
+                <CustomInput
+                  type="text"
                   className='w-full !h-[55px] !text-lg !mb-2.5 !-mt-2'
                   placeholder="Введите ваш email адрес"
                   value={email}
                   onChange={handleEmailChange}
-                  onPressEnter={handleResetPassword}
-                  status={error ? 'error' : ''}
                 />
+                {error && <p className="text-red-500">{error}</p>}
+
                 <Button
                   type='primary'
                   className=' !w-full !h-[55px] !text-sm !font-bold !uppercase'
